acf16f802665b51ef3a3e80955b6dfcf52a30194
yumaaji <yumaaji126@gmail.com>
2023-11-28 19:00:43 +0700

    menambahkan fitur search date pada Rekap Laporan

--- app/Views/rekaplaporan/index.php
+++ app/Views/rekaplaporan/index.php
@@ -16,5 +16,30 @@
       <div class="breadcrumb-item">Rekap Laporan</div>
     </div>
   </div>
+  </section>
+  <div class="date-container">
+    <div  class="date">
+      <div class="card">
+        <div class="card-body">
+          <div class="form-group">
+            <input type="date" class="form-control">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="date">
+      <div class="card">
+        <div class="card-body">
+          <div class="form-group">
+            <input type="date" class="form-control">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="button-result">
+      <a href="#" class="btn btn-lg btn-dark button"><i class="fa-solid fa-magnifying-glass"></i><span style="margin-left: 20px;">Cari</span></a>
+    </div> 
+
   </div>
   <?= $this->endSection() ?>
